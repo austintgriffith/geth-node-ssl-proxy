f33dcf5a4fe44201cc64b5b988f8b0c822d6819a
Ubuntu <ubuntu@ip-172-31-34-98.ec2.internal>
2025-03-17 19:50:40 +0000

    Fixed cache stale timeout

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ const poolPort = 3003;
 const fallbackRequestTimeout = 5000; // 15 seconds
 const cacheMaxRetries = 50;
 const cacheRetryDelay = 5000; // 5 seconds
-const cacheKeyTimeout = 1000; // 15 second timeout
+const cacheKeyTimeout = 15000; // 15 second timeout
 
 const fallbackRequestLogPath = "/home/ubuntu/shared/fallbackRequests.log";
 const cacheRequestLogPath = "/home/ubuntu/shared/cacheRequests.log";
