7a792606da5eaa3f44251b1885c1d2f2bc1cadbf
Ubuntu <ubuntu@ip-172-31-34-98.ec2.internal>
2025-03-17 17:36:56 +0000

    Decreased timeout for pool and fallback requests

--- config.js
+++ config.js
@@ -3,10 +3,10 @@ const cachePort = 3002;
 const proxyPortPublic = 48544;
 const webServerPort = 48545;
 const poolPort = 3003;
-const fallbackRequestTimeout = 15000; // 15 seconds
+const fallbackRequestTimeout = 5000; // 15 seconds
 const cacheMaxRetries = 50;
 const cacheRetryDelay = 5000; // 5 seconds
-const cacheKeyTimeout = 15000; // 15 second timeout
+const cacheKeyTimeout = 1000; // 15 second timeout
 
 const fallbackRequestLogPath = "/home/ubuntu/shared/fallbackRequests.log";
 const cacheRequestLogPath = "/home/ubuntu/shared/cacheRequests.log";
