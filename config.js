ba8a3ccedb19e21a21581b5a0d61159ee589ec76
Ubuntu <ubuntu@ip-172-31-34-98.ec2.internal>
2025-05-19 16:41:38 +0000

    Changed fallback bot alert threshold

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ const cacheMaxRetries = 50;
 const cacheRetryDelay = 5000; // 5 seconds
 const blockNumberCacheTimeout = 15000; // 15 second timeout
 const cacheMethodCleanupTimeout = 1000 * 60 * 60 * 6; // 6 hours
-const fallbackRateAlertThreshold = 1; // Fallback requests per hour to trigger telegram alert
+const fallbackRateAlertThreshold = 9; // Fallback requests per hour to trigger telegram alert
 
 const fallbackRequestLogPath = "/home/ubuntu/shared/fallbackRequests.log";
 const cacheRequestLogPath = "/home/ubuntu/shared/cacheRequests.log";
