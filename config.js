feb48bd1b498da3d3bf6d438a3ca9fe48266d306
Ubuntu <ubuntu@ip-172-31-89-145.ec2.internal>
2025-06-30 22:37:34 +0000

    Reduced cache cleanup time from 6 hours to 2

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ const fallbackRequestTimeout = 5000; // 5 seconds
 const cacheMaxRetries = 50;
 const cacheRetryDelay = 5000; // 5 seconds
 const blockNumberCacheTimeout = 19000; // 19 second timeout
-const cacheMethodCleanupTimeout = 1000 * 60 * 60 * 6; // 6 hours
+const cacheMethodCleanupTimeout = 1000 * 60 * 60 * 2; // 2 hours
 const fallbackRateAlertThreshold = 9; // Fallback requests per hour to trigger telegram alert
 
 const fallbackRequestLogPath = "/home/ubuntu/shared/fallbackRequests.log";
